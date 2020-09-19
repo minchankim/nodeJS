f94d9aba977730de5dbd5f58455495973bc4113e
minchankim <doridorism@naver.com>
2020-09-19 16:39:57 +0900

    commit on master!

--- test/test.js
+++ test/test.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function handler(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hellosdfsfdsfd dddddtttttdWorld\n');
-}).listen(1337, '61.72.75.77');
-console.log('Server running at http://61.72.75.77:1337/');
+}).listen(1337, '192.168.1.4');
+console.log('Server running at http://192.168.1.4:1337/');
