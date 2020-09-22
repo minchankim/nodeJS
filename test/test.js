b57c5cba60ce32e24ba70c7b3a3610756bf69623
minchankim <doridorism@naver.com>
2020-09-22 21:11:08 +0900

    0922

--- test/test.js
+++ test/test.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function handler(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hellosdfsfdsfd dddddtttttdWorld\n');
-}).listen(1337, '61.72.75.77');
-console.log('Server running at http://61.72.75.77:1337/');
+}).listen(1337, '125.128.24.156');
+console.log('Server running at http://125.128.24.156:1337/');
